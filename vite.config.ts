59bb9e2f29b52a6d3c13bf88cb1898152a4db823
Jerryh001 <jerryh001@hotmail.com>
2025-03-05 10:49:49 +0800

    refactor: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": resolve(import.meta.dirname, "src"),
+    },
+  },
   plugins: [react()],
-})
+});
